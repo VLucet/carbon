0217f2bd5367af1de5a7516d8b87c909cbf21ecb
Valentin <valentin.lucet@gmail.com>
2021-08-08 13:49:41 -0400

    updates

--- R/ppm_from_date.R
+++ R/ppm_from_date.R
@@ -1,4 +1,20 @@
 
+#' Get the CO2 ppm value closest to a given date
+#'
+#' Given a date, retrieves the atmospheric CO2 concentration value the closest
+#' to that date, using the data from the Mauna Loa observatory
+#'
+#' @param date A character string of the format "yyyy-mm-dd"
+#'
+#' @return
+#' A floating point value, the closest data point to the input date
+#'
+#' @references
+#' NOAA's [Global Monitoring Laboratory data hub](https://gml.noaa.gov/ccgg/trends/data.html).
+#'
+#' @examples
+#' ppm_from_date("1978-06-05")
+#'
 #' @export
 ppm_from_date <- function(date) {
 
